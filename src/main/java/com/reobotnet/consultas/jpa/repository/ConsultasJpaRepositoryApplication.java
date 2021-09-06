f9fb103a352ccb1a14c0070f5c9d8c8ebe00ddeb
FlavioFCarvalho <flaviocfr@gmail.com>
2021-09-06 12:56:52 -0300

    Open house

---
+++ src/main/java/com/reobotnet/consultas/jpa/repository/ConsultasJpaRepositoryApplication.java
@@ -0,0 +1,13 @@
+package com.reobotnet.consultas.jpa.repository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultasJpaRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultasJpaRepositoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reobotnet/consultas/pa/repository/ConsultasJpaRepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reobotnet.consultas.pa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultasJpaRepositoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
